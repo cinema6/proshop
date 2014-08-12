38ea39dd377e57b5e4b41c90f5cd1e860b42109f
Scott Munson <smunson@cinema6.com>
2014-08-12 12:27:42 -0400

    add an error to mock http requests

--- app/assets/scripts/mockHttpDefs.js
+++ app/assets/scripts/mockHttpDefs.js
@@ -137,7 +137,8 @@ define(['mockHttp'], function(mockHttp){
     });
 
     httpMocks.whenPUT('/api/account/user/e2e-12224', function() {
-        this.proxy('mocks/account/user/e2e-user.json');
+        this.respond(404, 'This User cannot be edited for some reason');
+        // this.proxy('mocks/account/user/e2e-user.json');
     });
 
     httpMocks.whenPUT('/api/account/user/e2e-12225', function() {
