3b39d682baf1a07605d67d9443269ad7dbdc0b43
Scott Munson <scott@thinkcontra.com>
2015-01-05 17:10:20 -0500

    add SiteController and split logic from SitesCtrl, add container logic

--- app/assets/scripts/sites.js
+++ app/assets/scripts/sites.js
@@ -2,8 +2,8 @@ define(['account'], function(account) {
     'use strict';
 
     return angular.module('c6.proshop.sites', [account.name])
-        .controller('SitesController', ['$scope','$log','SitesService','account','ConfirmDialogService','$q',
-        function                       ( $scope , $log , SitesService , account , ConfirmDialogService , $q ) {
+        .controller('SitesController', ['$scope','$location','$log','SitesService','account','ConfirmDialogService','$q',
+        function                       ( $scope , $location , $log , SitesService , account , ConfirmDialogService , $q ) {
             var self = this,
                 _data = {},
                 bindBrandToName = true;
@@ -13,6 +13,12 @@ define(['account'], function(account) {
 
             function convertNameToBrand(name) {
                 return name.toLowerCase().split(',')[0].replace(/ /g, '_');
+            }
+
+            function toTitleCase(str) {
+                return str.replace(/\w\S*/g, function(txt) {
+                    return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+                });
             }
 
             function initView() {
@@ -59,10 +65,6 @@ define(['account'], function(account) {
                 }
             });
 
-            self.disableBrandBinding = function() {
-                bindBrandToName = false;
-            };
-
             self.filterData = function(query) {
                 var _query = query.toLowerCase(),
                     orgs = _data.orgs.filter(function(org) {
@@ -92,81 +94,12 @@ define(['account'], function(account) {
                 self.org = !site.org ? null : self.orgs.filter(function(org) {
                     return site.org.id === org.id;
                 })[0];
+                enableActiveContainers(site.containers);
                 self.action = 'edit';
             };
 
             self.addNewSite = function() {
-                $scope.message = null;
-                self.site = {
-                    status: 'active'
-                };
-                self.org = null;
-                self.action = 'new';
-            };
-
-            self.saveSite = function(site) {
-                var s = {};
-
-                function handleError(err) {
-                    $log.error(err);
-                    ConfirmDialogService.display({
-                        prompt: 'There was a problem saving the Site. ' + err + '.',
-                        affirm: 'Close',
-                        onAffirm: function() {
-                            ConfirmDialogService.close();
-                        }
-                    });
-                }
-
-                function handleSuccess(site) {
-                    $log.info('saved user: ', site);
-                    $scope.message = 'Successfully saved Site: ' + site.name;
-                    initView();
-                    self.action = 'all';
-                }
-
-                s.org = self.org ? self.org.id : null;
-
-                ['name','branding','host','status','placementId','wildCardPlacement'].forEach(function(prop) {
-                    s[prop] = site[prop];
-                });
-
-                if (site.id) {
-                    SitesService.putSite(site.id, s)
-                        .then(handleSuccess, handleError);
-                } else {
-                    SitesService.postSite(s)
-                        .then(handleSuccess, handleError);
-                }
-            };
-
-            self.confirmDelete = function() {
-                ConfirmDialogService.display({
-                    prompt: 'Are you sure you want to delete this Site?',
-                    affirm: 'Yes',
-                    cancel: 'Cancel',
-                    onAffirm: function() {
-                        ConfirmDialogService.close();
-                        SitesService.deleteSite(self.site.id)
-                            .then(function() {
-                                $scope.message = 'Successfully deleted Site: ' + self.site.name;
-                                initView();
-                                self.action = 'all';
-                            }, function(err) {
-                                $log.error(err);
-                                ConfirmDialogService.display({
-                                    prompt: 'There was a problem deleting the Site. ' + err + '.',
-                                    affirm: 'Close',
-                                    onAffirm: function() {
-                                        ConfirmDialogService.close();
-                                    }
-                                });
-                            });
-                    },
-                    onCancel: function() {
-                        ConfirmDialogService.close();
-                    }
-                });
+                $location.path('/sites/new');
             };
 
             $scope.tableHeaders = [
@@ -195,44 +128,201 @@ define(['account'], function(account) {
                 }
             };
 
+            initView();
+
+        }])
+
+        .controller('SiteController', ['$scope','$routeParams','$location','$log','$q','SitesService','account','ConfirmDialogService',
+        function                      ( $scope , $routeParams , $location , $log , $q , SitesService , account , ConfirmDialogService ) {
+            var self = this,
+                bindBrandToName = true;
+
+            $log = $log.context('SiteCtrl');
+            $log.info('instantiated');
+
+            function convertNameToBrand(name) {
+                return name.toLowerCase().split(',')[0].replace(/ /g, '_');
+            }
+
+            function toTitleCase(str) {
+                return str.replace(/\w\S*/g, function(txt) {
+                    return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+                });
+            }
+
+            function initView() {
+                var promiseArray = [account.getOrgs()];
+
+                self.loading = true;
+
+                if ($routeParams.id) {
+                    promiseArray.push(SitesService.getSite($routeParams.id));
+                }
+
+                $q.all(promiseArray)
+                    .then(function(promises) {
+                        var orgs = promises[0],
+                            site = promises[1] || {
+                                status: 'active',
+                                containers: []
+                            };
+
+                        self.orgs = orgs;
+                        self.site = site;
+                        self.org = !site.org ? null : orgs.filter(function(org) {
+                            return site.org === org.id;
+                        })[0];
+
+                        enableActiveContainers(site.containers);
+
+                        self.loading = false;
+                    });
+            }
+
+            self.containers = [
+                {type: 'embed', name: 'Embed', enabled: false},
+                {type: 'mr2', name: 'MR2 Widget', enabled: false},
+                {type: 'taboola', name: 'Taboola', enabled: false},
+                {type: 'outbrain', name: 'Outbrain', enabled: false},
+                {type: 'veeseo', name: 'Veeseo', enabled: false},
+                {type: '', name: 'Other', enabled: false}
+            ];
+
+            function enableActiveContainers(containers) {
+                if (!containers) { return; }
+
+                var types = self.containers.reduce(function(result, container) {
+                    return result.concat(container.type);
+                },[]);
+
+                containers.forEach(function(siteContainer) {
+                    if (types.indexOf(siteContainer.type) > -1) {
+                        self.containers.forEach(function(ctrlContainer) {
+                            if (siteContainer.type === ctrlContainer.type) {
+                                ctrlContainer.enabled = true;
+                                angular.extend(ctrlContainer, siteContainer);
+                            }
+                        });
+                    } else {
+                        self.containers.push(angular.extend({
+                            name: toTitleCase(siteContainer.type.replace(/_/g, ' ')),
+                            enabled: true
+                        }, siteContainer));
+                    }
+                });
+            }
+
+            function convertContainersForSaving(containers) {
+                var _containers = [],
+                    types = self.containers.reduce(function(result, container) {
+                        return container.enabled && result.indexOf(container.type) < 0 ? result.concat(container.type) : result;
+                    }, []);
+
+                containers.forEach(function(container) {
+                    var index = types.indexOf(container.type);
+
+                    if (index > -1) {
+                        types.splice(index, 1);
+                        _containers.push(container);
+                    }
+                });
+
+                types.forEach(function(type) {
+                    _containers.push({ type: type });
+                });
+
+                return _containers;
+            }
+
+            self.addContainerItem = function(container) {
+                if (container.name === 'Other') {
+                    self.containers.push({
+                        type: container.type.replace(/ /g, '_').toLowerCase(),
+                        name: toTitleCase(container.type),
+                        enabled: true
+                    });
+                    container.type = '';
+                } else {
+                    container.enabled = true;
+                }
+            };
+
+            self.disableBrandBinding = function() {
+                bindBrandToName = false;
+            };
+
+            self.saveSite = function(site) {
+                var s = {};
+
+                function handleError(err) {
+                    $log.error(err);
+                    ConfirmDialogService.display({
+                        prompt: 'There was a problem saving the Site. ' + err + '.',
+                        affirm: 'Close',
+                        onAffirm: function() {
+                            ConfirmDialogService.close();
+                        }
+                    });
+                }
+
+                function handleSuccess(site) {
+                    $log.info('saved user: ', site);
+                    $scope.message = 'Successfully saved Site: ' + site.name;
+                    $location.path('/sites');
+                }
+
+                s.org = self.org ? self.org.id : null;
+
+                ['name','branding','host','status','placementId','wildCardPlacement'].forEach(function(prop) {
+                    s[prop] = site[prop];
+                });
+
+                s.containers = convertContainersForSaving(site.containers);
+
+                if (site.id) {
+                    SitesService.putSite(site.id, s)
+                        .then(handleSuccess, handleError);
+                } else {
+                    SitesService.postSite(s)
+                        .then(handleSuccess, handleError);
+                }
+            };
+
+            self.confirmDelete = function() {
+                ConfirmDialogService.display({
+                    prompt: 'Are you sure you want to delete this Site?',
+                    affirm: 'Yes',
+                    cancel: 'Cancel',
+                    onAffirm: function() {
+                        ConfirmDialogService.close();
+                        SitesService.deleteSite(self.site.id)
+                            .then(function() {
+                                $scope.message = 'Successfully deleted Site: ' + self.site.name;
+                                $location.path('/sites');
+                            }, function(err) {
+                                $log.error(err);
+                                ConfirmDialogService.display({
+                                    prompt: 'There was a problem deleting the Site. ' + err + '.',
+                                    affirm: 'Close',
+                                    onAffirm: function() {
+                                        ConfirmDialogService.close();
+                                    }
+                                });
+                            });
+                    },
+                    onCancel: function() {
+                        ConfirmDialogService.close();
+                    }
+                });
+            };
+
             $scope.$watch(function() {return self.site && self.site.name;}, function(newName) {
-                if (newName && bindBrandToName && self.action === 'new') {
+                if (newName && bindBrandToName) {
                     self.site.branding = convertNameToBrand(newName);
                 }
             });
 
             initView();
-
-        }])
-
-        .directive('sitesAll', [ function ( ) {
-            return {
-                restrict: 'E',
-                templateUrl: 'views/sites/sites_all.html',
-                link: function(/*scope, element, attrs, ctrl*/) {
-                    // can move any DOM stuff from Ctrl into here...
-                }
-            };
-        }])
-
-        .directive('sitesEdit', [ function ( ) {
-            return {
-                restrict: 'E',
-                templateUrl: 'views/sites/sites_edit.html',
-                link: function(/*scope, element, attrs, ctrl*/) {
-                    // can move any DOM stuff from Ctrl into here...
-                }
-            };
-        }])
-
-        .directive('sitesNew', [ function ( ) {
-            return {
-                restrict: 'E',
-                templateUrl: 'views/sites/sites_edit.html',
-                link: function(/*scope, element, attrs, ctrl*/) {
-                    // can move any DOM stuff from Ctrl into here...
-                }
-            };
         }])
 
         .service('SitesService', ['c6UrlMaker','$http','$q','$timeout',
