2204df76ac1d9f62d3ca7029be80644b6f0fd973
Scott Munson <smunson@cinema6.com>
2015-01-08 11:44:41 -0500

    add scripting for categories

--- app/assets/scripts/categories.js
+++ app/assets/scripts/categories.js
@@ -2,8 +2,8 @@ define([], function() {
     'use strict';
 
     return angular.module('c6.proshop.categories',[])
-        .controller('CategoriesController', ['$scope','$log','ConfirmDialogService','$q','$location',
-        function                            ( $scope , $log , ConfirmDialogService , $q , $location ) {
+        .controller('CategoriesController', ['$scope','$log','$location','CategoriesService',
+        function                            ( $scope , $log , $location , CategoriesService ) {
             var self = this,
                 _data = {};
 
@@ -11,7 +11,15 @@ define([], function() {
             $log.info('instantiated');
 
             function initView() {
+                self.loading = true;
 
+                CategoriesService.getCategories()
+                    .then(function(categories) {
+                        self.categories = categories;
+                    })
+                    .finally(function() {
+                        self.loading = false;
+                    });
             }
 
             self.query = null;
@@ -54,18 +62,14 @@ define([], function() {
             };
 
             $scope.tableHeaders = [
+                {label:'Label',value:'label'},
                 {label:'Name',value:'name'},
-                {label:'Domain',value:'host'},
-                {label:'Org',value:'org.name'},
-                {label:'AdTech ID',value:'placementId'},
-                {label:'Wild Card ID',value:'wildCardPlacement'},
-                {label:'Branding',value:'branding'},
                 {label:'Status',value:'status'},
                 {label:'Last Updated',value:'lastUpdated'}
             ];
 
             $scope.sort = {
-                column: 'name',
+                column: 'label',
                 descending: false
             };
 
@@ -78,17 +82,94 @@ define([], function() {
                     sort.descending = false;
                 }
             };
+
+            initView();
         }])
-        .controller('CategoryController', [function() {
+        .controller('CategoryController', ['$scope','$log','ConfirmDialogService','$q','$location','CategoriesService','$routeParams',
+        function                          ( $scope , $log , ConfirmDialogService , $q , $location , CategoriesService , $routeParams ) {
             var self = this;
 
-            self.save = function() {
+            function initView() {
 
+                if ($routeParams.id) {
+                    self.loading = true;
+
+                    CategoriesService.getCategory($routeParams.id)
+                        .then(function(category) {
+                            self.category = category;
+                        })
+                        .finally(function() {
+                            self.loading = false;
+                        });
+                } else {
+                    self.category = {
+                        status: 'active'
+                    };
+                }
+            }
+
+            self.save = function(category) {
+                var c = {};
+
+                function handleError(err) {
+                    $log.error(err);
+                    ConfirmDialogService.display({
+                        prompt: 'There was a problem saving the Category. ' + err + '.',
+                        affirm: 'Close',
+                        onAffirm: function() {
+                            ConfirmDialogService.close();
+                        }
+                    });
+                }
+
+                function handleSuccess(category) {
+                    $log.info('saved user: ', category);
+                    $scope.message = 'Successfully saved Category: ' + category.name;
+                    $location.path('/categories');
+                }
+
+                ['name','label','status'].forEach(function(prop) {
+                    c[prop] = category[prop];
+                });
+
+                if (category.id) {
+                    CategoriesService.putCategory(category.id, c)
+                        .then(handleSuccess, handleError);
+                } else {
+                    CategoriesService.postCategory(c)
+                        .then(handleSuccess, handleError);
+                }
             };
 
             self.delete = function() {
-
+                ConfirmDialogService.display({
+                    prompt: 'Are you sure you want to delete this Category?',
+                    affirm: 'Yes',
+                    cancel: 'Cancel',
+                    onAffirm: function() {
+                        ConfirmDialogService.close();
+                        CategoriesService.deleteCategory(self.category.id)
+                            .then(function() {
+                                $scope.message = 'Successfully deleted Category: ' + self.category.name;
+                                $location.path('/categories');
+                            }, function(err) {
+                                $log.error(err);
+                                ConfirmDialogService.display({
+                                    prompt: 'There was a problem deleting the Category. ' + err + '.',
+                                    affirm: 'Close',
+                                    onAffirm: function() {
+                                        ConfirmDialogService.close();
+                                    }
+                                });
+                            });
+                    },
+                    onCancel: function() {
+                        ConfirmDialogService.close();
+                    }
+                });
             };
+
+            initView();
         }])
         .service('CategoriesService', ['c6UrlMaker','$http','$q','$timeout',
         function                      ( c6UrlMaker , $http , $q , $timeout ) {
@@ -120,17 +201,24 @@ define([], function() {
                 return deferred.promise;
             }
 
+            this.getCategories = function(param, value) {
+                return httpWrapper({
+                    method: 'GET',
+                    url: c6UrlMaker('categories' + (param && value ? '?' + param + '=' + value : ''), 'api')
+                });
+            };
+
             this.getCategory = function(id) {
                 return httpWrapper({
                     method: 'GET',
-                    url: c6UrlMaker('categories/' + id, 'api')
+                    url: c6UrlMaker('category/' + id, 'api')
                 });
             };
 
             this.putCategory = function(id, cat) {
                 return httpWrapper({
                     method: 'PUT',
-                    url: c6UrlMaker('categories/' + id, 'api'),
+                    url: c6UrlMaker('category/' + id, 'api'),
                     data: cat
                 });
             };
@@ -138,7 +226,7 @@ define([], function() {
             this.postCategory = function(cat) {
                 return httpWrapper({
                     method: 'POST',
-                    url: c6UrlMaker('categories', 'api'),
+                    url: c6UrlMaker('category', 'api'),
                     data: cat
                 });
             };
@@ -146,7 +234,7 @@ define([], function() {
             this.deleteCategory = function(id) {
                 return httpWrapper({
                     method: 'DELETE',
-                    url: c6UrlMaker('categories/' + id, 'api')
+                    url: c6UrlMaker('category/' + id, 'api')
                 });
             };
         }]);
